cf54bcc1a4acc2f7934af25bf1485c32418f3bf7
Haydar Pehlivan <h.pehlivan@student.avans.nl>
2020-12-09 19:50:13 +0100

    fix wall collide error

--- 02_CODE_GameLib/Game.cs
+++ 02_CODE_GameLib/Game.cs
@@ -24,13 +24,13 @@ namespace CODE_GameLib
 
         public void MovePlayer(WindRose direction)
         {
-            if (Player.X >= CurrentRoom.Width)
+            if (Player.X >= CurrentRoom.Width - 1)
                 Player.Move(WindRose.West);
-            else if (Player.Y >= CurrentRoom.Height)
+            else if (Player.Y >= CurrentRoom.Height - 1)
                 Player.Move(WindRose.North);
-            else if (Player.X <= -1)
+            else if (Player.X <= 0)
                 Player.Move(WindRose.East);
-            else if (Player.Y <= -1)
+            else if (Player.Y <= 0)
                 Player.Move(WindRose.South);
             else
                 Player.Move(direction);
